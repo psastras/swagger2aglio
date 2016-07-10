6f5bd31136433dd1ef5230d2a1a93610616dc368
Paul Sastrasinh <psastras@gmail.com>
2016-07-10 03:32:27 -0400

    fix(ci): add simple theme test

--- test.js
+++ test.js
@@ -2,9 +2,24 @@ var assert = require('assert');
 var swagger2aglio = require('./index');
 
 describe('Swagger documentation generator', function () {
+
+  var petstorePath = './petstore_expanded.yml'; 
+
   it ('should run with minimal arguments', function (done) {
     swagger2aglio.convert({
-      input: './petstore_expanded.yml'
+      input: petstorePath
+    }, function (err, html) {
+      assert.ifError(err);
+      assert(html);
+      done();
+    });
+  });
+
+  it ('should accept theme parameters', function (done) {
+    swagger2aglio.convert({ 
+      input: petstorePath,
+      themeTemplate: 'triple',
+      themeVariables: 'slate',
     }, function (err, html) {
       assert.ifError(err);
       assert(html);
